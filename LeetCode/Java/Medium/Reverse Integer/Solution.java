0a6fb29cb8d10a468780d0e0b297412b8d707130
SatyaBoo <satyaxboo@gmail.com>
2026-07-18 11:04:30 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/Reverse Integer/Solution.java
+++ LeetCode/Java/Medium/Reverse Integer/Solution.java
@@ -1,5 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        
+        long rev=0;
+
+        while(x != 0){
+            rev = rev*10 + (x%10);
+            x/=10;
+        }
+        if(Integer.MAX_VALUE < rev || Integer.MIN_VALUE > rev)
+        return 0;
+
+        return (int)rev;
     }
 }
